4eae948df0b67189d64cce0eac5f77625acf21ea
Kai Kremer <kremerkp@gmail.com>
2019-09-20 15:13:34 +0200

    add redux codepen example plus reducers to blog

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,11 +1,16 @@
-import jsonPlaceholder from '../apis/jsonPlaceholder';
+import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-
-export const fetchPosts =  () => {
-    const promise =  jsonPlaceholder.get('/posts');
-    return {
-        type: 'FETCH_POSTS',
-        payload: promise
-    };
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
 
+/*  
+    export const fetchPosts = () => {
+    return function(dispatch, getStaet) {
+        const promise = jsonPlaceholder.get("/posts");
+        dispatch({ tpye: "FETCH_POSTS", payload: promise });
+    };
+
+    };
+*/
