8f25786364ee131dae2aff9fae9185f705296fbc
Kai Kremer <kai.kremer@eye-t.local>
2019-09-24 08:43:24 +0200

    commit test

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,9 +3,10 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response });
+  // test commit
 };
 
-/*  
+/*
     export const fetchPosts = () => {
     return function(dispatch, getStaet) {
         const promise = jsonPlaceholder.get("/posts");
